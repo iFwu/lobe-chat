7ee1d9763fe9143424d3aee72a84188d973b7f55
iFwu <blanu@live.com>
2024-11-01 16:06:44 +0800

    feat: add eslint and typescript ignore options in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,10 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH;
 const nextConfig = {
   basePath,
   compress: isProd,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   experimental: {
     optimizePackageImports: [
       'emoji-mart',
@@ -103,9 +107,9 @@ const nextConfig = {
       },
     ];
   },
-
   output: buildWithDocker ? 'standalone' : undefined,
   reactStrictMode: true,
+
   redirects: async () => [
     {
       destination: '/sitemap-index.xml',
@@ -160,13 +164,14 @@ const nextConfig = {
       source: '/settings',
     },
   ],
-
   rewrites: async () => [
     // due to google api not work correct in some countries
     // we need a proxy to bypass the restriction
     { destination: `${API_PROXY_ENDPOINT}/api/chat/google`, source: '/api/chat/google' },
   ],
-
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   webpack(config) {
     config.experiments = {
       asyncWebAssembly: true,
